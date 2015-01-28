dfc1a18f29e4b4c9b80a7b2713176ec56af9a6a3
Evan Moore <etmoore@gmail.com>
2015-01-28 10:37:54 -0700

    added title property to note -- needs styling

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -3,10 +3,11 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   actions: {
     newNote: function() {
-      var body = this.get('noteCopy');
-
-      var note = this.store.createRecord('note', { body: body });
-      this.set('noteCopy', '');
+      var body = this.get('noteBody');
+      var title = this.get('noteTitle');
+      var note = this.store.createRecord('note', { body: body , title: title});
+      this.set('noteTitle', '');
+      this.set('noteBody', '');
       note.save();
     },
     deleteNote: function(id) {
