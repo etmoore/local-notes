5c967e37a6d696541460e4b94ace661cbb555948
Evan Moore <etmoore@gmail.com>
2015-01-31 17:37:13 -0700

    removed created at and updated at attribetes from note

--- app/models/note.js
+++ app/models/note.js
@@ -3,6 +3,4 @@ import DS from "ember-data";
 export default DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
-  created_at: DS.attr('date'),
-  updated_at: DS.attr('date')
 });
